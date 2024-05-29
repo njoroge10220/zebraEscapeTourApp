cb84199730927ecc6bf63c8f3e232f15e6d3a2f2
njoroge10220 <jmnjoroge13@gmail.com>
2024-05-29 12:41:52 +0300

    git commit -m

--- tour/views.py
+++ tour/views.py
@@ -147,19 +147,35 @@ def register(request):
 
         form = 0   # 1 2 3 - username  4 5 - email  6 7 - password  8 9 - confirm password
         if username=='' or email=='' or password=='' or con_password=='':
-            form = 0
+            form = 0 # fill in all the required details
         else:    
             if len(username) < 4:
-                form = 1 # username is short
+                form = 1 # enter a complete username of atleast 4 characters
             else: # name should not be matching another in the db
                 for user in  Regular_Users:
                     if username == user.username:
-                        form = 2 # username is taken
-                                  
+                        form = 2 # username is taken try another username
+                    else:
+                        form = 3 # username is okay continue
+                
+            if '@' in email:
+                form = 4 # email is okay continue
+            else:
+                form = 5 # email requires @
             
+            if len(password) < 5:
+                form = 6 # enter a complete password of atleast 5 characters
+            else:
+                form = 7 # password is okay continue
+                
+            if con_password != password:
+                form = 8 # con_password is not same to password
+            else:
+                form = 9 # confirmation password is okay continue to save
         
-        new_user = Regular_User(username=username, email=email, password=password, con_password=con_password)
-        new_user.save() 
+        if form != 0 or form != 1 or form != 2 or form != 5 or form != 6 or form != 8:        
+            new_user = Regular_User(username=username, email=email, password=password, con_password=con_password)
+            new_user.save() 
                
         return render(request, 'register.html', {'form' : form, 'Places': Places, 'Pictures': Pictures, 'Listings' : Listings, 'Contacts': Contacts, 'Website_Images': Website_Images})          
     
@@ -193,8 +209,7 @@ def unregister(request):
                                   
             
         
-        new_user = Regular_User(username=username, email=email, password=password, con_password=con_password)
-        new_user.save() 
+
                
         return render(request, 'unregister.html', {'form' : form, 'Places': Places, 'Pictures': Pictures, 'Listings' : Listings, 'Contacts': Contacts, 'Website_Images': Website_Images})          
     
